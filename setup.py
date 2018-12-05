540c24b89490517c6aa3636a37122f3777ed0196
alex <vlasyuk.alexander@gmail.com>
2018-12-05 19:28:17 +0300

    create setup.py describing a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='onederx',
+    version='0.0.1',
+    description='REST and Websockets API for Onederx - Crypto derivatives exchange',
+    license='MIT',
+    packages=['onederx'],
+    author='Onederx',
+    keywords=['onederx', 'onederx-api', 'futures', 'derivative', 'leverage', 'client', 'api', 'trade', 'bitcoin', 'BTC', 'ethereum', 'ETH', 'wrapper', 'exchange', 'crypto', 'currency', 'trading', 'trading-api'],
+    url='https://github.com/onederx/onederx-python'
+)
