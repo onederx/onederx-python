7666efcb1681245fd3bd4014a2348bb35b7fdca2
alex <vlasyuk.alexander@gmail.com>
2019-04-15 01:11:34 +0300

    new version for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='onederx',
-    version='0.0.3',
+    version='0.0.4',
     description='REST and Websockets API for Onederx - Crypto derivatives exchange',
     license='MIT',
     packages=['onederx'],
     install_requires=['websockets >= 6.0', 'requests'],
     author='Onederx',
-    keywords=['onederx', 'onederx-api', 'futures', 'derivative', 'leverage', 'client', 'api', 'trade', 'bitcoin', 'BTC', 'ethereum', 'ETH', 'wrapper', 'exchange', 'crypto', 'currency', 'trading', 'trading-api'],
+    keywords=['onederx', 'onederx-api', 'futures', 'derivative', 'leverage', 'client', 'api', 'trade', 'bitcoin', 'BTC', 'ethereum', 'ETH', 'wrapper', 'exchange', 'crypto', 'currency', 'trading', 'trading-api', 'memes'],
     url='https://github.com/onederx/onederx-python'
 )
